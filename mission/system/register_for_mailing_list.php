8f7ffcbc3be2f187a470a16fb7993015de1df002
Alex Heritier <alex.heritier@gmail.com>
2016-04-12 20:26:52 -0700

    Added robots.txt and updated the mailing script so that hopefully it won't get set off accidentally anymore

--- mission/system/register_for_mailing_list.php
+++ mission/system/register_for_mailing_list.php
@@ -1,15 +1,14 @@
 <?php
 
-$email = $_GET['email'];
+if (isset($_GET['email']) && $_GET['email'] != "") {
+    $email = $_GET['email'];
+    $email_list = fopen("../../resources/mailing_list.txt", "a");
+    fwrite($email_list, $email . "\n");
+    fclose($email_list);
 
-$email_list = fopen("../../resources/mailing_list.txt", "a");
-fwrite($email_list, $email . "\n");
-fclose($email_list);
+    $subject = 'New mailing recipient';
+    $body = $email . ' has signed up for the mailing list.' . ' The entire mailing list is at http://www.moonfoxfarm.com/resources/mailing_list.txt';
 
-$subject = 'New mailing recipient';
-$body = $email . ' has signed up for the mailing list.\n\n' . 'The entire mailing list is at http://moonfoxfarm.com/resources/mailing_list.txt';
-	
-mail('moonfoxfarm@gmail.com', $subject, $body);
-
-
-?>
+    mail('moonfoxfarm@gmail.com', $subject, $body);
+    #mail('alex.heritier@gmail.com', $subject, $body);
+}
